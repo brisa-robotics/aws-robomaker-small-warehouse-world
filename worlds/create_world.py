91648b462aa33ae9422f835d3f67e700862972aa
David Bensoussan <minipada@gmail.com>
2020-08-17 19:26:39 +0200

    conf: more worlds

--- worlds/create_world.py
+++ worlds/create_world.py
@@ -4,6 +4,7 @@ import argparse
 import copy
 import xml.etree.ElementTree as et
 
+SPACE_CORRIDORS = 3
 GROUND_X = 13.9805
 GROUND_Y = 20.9067
 names_to_remove = [
@@ -15,6 +16,62 @@ names_to_remove = [
     "aws_robomaker_warehouse_Bucket_01_022",
     "aws_robomaker_warehouse_PalletJackB_01_001",
 ]
+
+
+def create_no_roof_no_walls_small_warehouse_9_corner():
+    tree = et.parse("no_roof_small_warehouse.world")
+    root = tree.getroot()
+    root_copy = copy.deepcopy(root)
+
+    xml_header = "<?xml version='1.0' encoding='utf-8'?>"
+
+    for element in root.iter():
+        if element.tag in ["model", "light"]:
+            if element.tag not in ["gravity", "physics", "gui"] and element.attrib["name"] in [
+                "aws_robomaker_warehouse_ShelfF_01_001",
+                "Warehouse_CeilingLight_003",
+                "aws_robomaker_warehouse_Lamp_01_005",
+            ]:
+                for x_count, x_val in enumerate([0, GROUND_X, 2 * (GROUND_X)]):
+                    for y_count, y_val in enumerate(
+                        [0, GROUND_Y + SPACE_CORRIDORS, 2 * (GROUND_Y + SPACE_CORRIDORS)]
+                    ):
+                        if x_count + y_count != 0:
+                            new_model = copy.deepcopy(element)
+                            new_model.set("name", f"{new_model.attrib['name']}_{x_count}_{y_count}")
+                            new_model_pose = new_model.find("pose").text.split(" ")
+                            new_model_pose[0] = str(float(new_model_pose[0]) + x_val)
+                            new_model_pose[1] = str(float(new_model_pose[1]) + y_val)
+                            new_model.find("pose").text = " ".join(new_model_pose)
+                            root_copy.find("world").append(new_model)
+
+    for element in root.iter():
+        if element.tag in ["model", "light"]:
+            if (
+                element.tag not in ["gravity", "physics", "gui"]
+                and element.attrib["name"] == "aws_robomaker_warehouse_GroundB_01_001"
+            ):
+                for x_count, x_val in enumerate([0, -GROUND_X, GROUND_X, 2 * GROUND_X, 3 * GROUND_X]):
+                    for y_count, y_val in enumerate(
+                        [0, -GROUND_Y, GROUND_Y, 2 * GROUND_Y, 3* GROUND_Y]
+                    ):
+                        if x_count + y_count != 0:
+                            new_model = copy.deepcopy(element)
+                            new_model.set("name", f"{new_model.attrib['name']}_{x_count}_{y_count}")
+                            new_model_pose = new_model.find("pose").text.split(" ")
+                            new_model_pose[0] = str(float(new_model_pose[0]) + x_val)
+                            new_model_pose[1] = str(float(new_model_pose[1]) + y_val)
+                            new_model.find("pose").text = " ".join(new_model_pose)
+                            root_copy.find("world").append(new_model)
+
+    for name in names_to_remove:
+        item = root_copy.find(f".//*[@name='{name}']")
+        root_copy.find("world").remove(item)
+
+    xml = xml_header + et.tostring(root_copy).decode("utf-8")
+
+    with open("no_roof_no_walls_small_warehouse_9_corner.world", "w") as out:
+        out.write(xml)
 
 
 def create_no_roof_no_walls_small_warehouse_9():
@@ -30,7 +87,9 @@ def create_no_roof_no_walls_small_warehouse_9():
                 element.tag not in ["gravity", "physics", "gui"]
                 and element.attrib["name"] not in names_to_remove
             ):
-                for x_count, x_val in enumerate([0, GROUND_X, -GROUND_X]):
+                for x_count, x_val in enumerate(
+                    [0, GROUND_X + SPACE_CORRIDORS, -GROUND_X - SPACE_CORRIDORS]
+                ):
                     for y_count, y_val in enumerate([0, GROUND_Y, -GROUND_Y]):
                         if x_count + y_count != 0:
                             new_model = copy.deepcopy(element)
@@ -65,7 +124,13 @@ def create_no_roof_no_walls_small_warehouse_25():
                 and element.attrib["name"] not in names_to_remove
             ):
                 for x_count, x_val in enumerate(
-                    [0, GROUND_X, -GROUND_X, 2 * GROUND_X, -2 * GROUND_X]
+                    [
+                        0,
+                        GROUND_X + SPACE_CORRIDORS,
+                        -GROUND_X - SPACE_CORRIDORS,
+                        2 * (GROUND_X + SPACE_CORRIDORS),
+                        -2 * (GROUND_X + SPACE_CORRIDORS),
+                    ]
                 ):
                     for y_count, y_val in enumerate(
                         [0, GROUND_Y, -GROUND_Y, 2 * GROUND_Y, -2 * GROUND_Y]
@@ -105,7 +170,7 @@ def create_no_roof_no_walls_small_warehouse_9_only_racks():
                 "aws_robomaker_warehouse_GroundB_01_001",
                 "aws_robomaker_warehouse_Lamp_01_005",
             ]:
-                for x_count, x_val in enumerate([0, GROUND_X, -GROUND_X]):
+                for x_count, x_val in enumerate([0, GROUND_X + SPACE_CORRIDORS]):
                     for y_count, y_val in enumerate([0, GROUND_Y, -GROUND_Y]):
                         if x_count + y_count != 0:
                             new_model = copy.deepcopy(element)
@@ -128,6 +193,7 @@ def create_no_roof_no_walls_small_warehouse_9_only_racks():
     with open("no_roof_no_walls_small_warehouse_9_only_racks.world", "w") as out:
         out.write(xml)
 
+
 def create_no_roof_no_walls_small_warehouse_25_only_racks():
     tree = et.parse("no_roof_small_warehouse.world")
     root = tree.getroot()
@@ -145,7 +211,13 @@ def create_no_roof_no_walls_small_warehouse_25_only_racks():
                 "aws_robomaker_warehouse_Lamp_01_005",
             ]:
                 for x_count, x_val in enumerate(
-                    [0, GROUND_X, -GROUND_X, 2 * GROUND_X, -2 * GROUND_X]
+                    [
+                        0,
+                        GROUND_X + SPACE_CORRIDORS,
+                        -GROUND_X - SPACE_CORRIDORS,
+                        2 * (GROUND_X + SPACE_CORRIDORS),
+                        -2 * (GROUND_X + SPACE_CORRIDORS),
+                    ]
                 ):
                     for y_count, y_val in enumerate(
                         [0, GROUND_Y, -GROUND_Y, 2 * GROUND_Y, -2 * GROUND_Y]
@@ -171,6 +243,7 @@ def create_no_roof_no_walls_small_warehouse_25_only_racks():
     with open("no_roof_no_walls_small_warehouse_25_only_racks.world", "w") as out:
         out.write(xml)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -183,6 +256,8 @@ if __name__ == "__main__":
 
     if args.world_name == "no_roof_no_walls_small_warehouse_9":
         create_no_roof_no_walls_small_warehouse_9()
+    elif args.world_name == "no_roof_no_walls_small_warehouse_9_corner":
+        create_no_roof_no_walls_small_warehouse_9_corner()
     elif args.world_name == "no_roof_no_walls_small_warehouse_25":
         create_no_roof_no_walls_small_warehouse_25()
     elif args.world_name == "no_roof_no_walls_small_warehouse_9_only_racks":
